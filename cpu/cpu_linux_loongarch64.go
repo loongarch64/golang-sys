ee2ba640d07e12da21f8dde9257e0a1da65e502e
Xiaotian Wu <wuxiaotian@loongson.cn>
2021-07-20 18:52:02 +0800

    update

--- cpu/cpu_linux_loongarch64.go
+++ cpu/cpu_linux_loongarch64.go
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux && loongarch64
-// +build linux
-// +build loongarch64
-
 package cpu
 
 // HWCAP/HWCAP2 bits. These are exposed by the Linux kernel.
